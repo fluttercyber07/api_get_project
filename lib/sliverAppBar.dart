966a6d32ffe4b3dcb85ad6c1a6863594fa35b6b9
fluttercyber07 <fluttercyber07@gmail.com>
2023-11-06 17:01:43 +0500

    sfwetergf

--- lib/sliverAppBar.dart
+++ lib/sliverAppBar.dart
@@ -10,7 +10,6 @@ class SliverAppBarExample extends StatefulWidget {
 class _SliverAppBarExampleState extends State<SliverAppBarExample> {
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
@@ -37,17 +36,191 @@ class _SliverAppBarExampleState extends State<SliverAppBarExample> {
               ),
             ),
           ),
-
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                  (context, index) => ListTile(
-                title: Text('Item $index'),
-              ),
+          // SliverList(
+          //   delegate: SliverChildBuilderDelegate(
+          //     (context, index) => ListTile(
+          //       title: Text('Item $index'),
+          //     ),
+          //   ),
+          // ),
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
             ),
+            delegate:
+                SliverChildBuilderDelegate((BuildContext context, int index) {
+              return Container(
+                height: 200,
+                margin: EdgeInsets.all(5),
+                color: Colors.red[(index % 9 + 1) * 100],
+                child: Column(
+                  children: [
+                    Image(
+                      image: AssetImage("assets/images/img.png"),
+                      height: 100,
+                      width: 150,
+                    ),
+                    Text("${index + 1} - widget "),
+                  ],
+                ),
+              );
+            }, childCount: 14),
+          ),
+
+          //////
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('grid item $index'),
+                );
+              },
+              childCount: 20,
+            ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 15,
+              crossAxisSpacing: 15,
+              childAspectRatio: 2.0,
+            ),
+          ),
+
+          /////
+          SliverToBoxAdapter(
+            child: Container(
+              color: Colors.yellow,
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Grid Header', style: TextStyle(fontSize: 24)),
+            ),
+          ),
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('grid item $index'),
+                );
+              },
+              childCount: 20,
+            ),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 15,
+              crossAxisSpacing: 15,
+              childAspectRatio: 2.0,
+            ),
+          ),
+
+
+          ///////////////
+          SliverGrid.count(
+            crossAxisCount: 3,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 4.0,
+            children: <Widget>[
+              Container(color: Colors.red),
+              Container(color: Colors.red),
+              Container(color: Colors.green),
+              Container(color: Colors.green),
+              Container(color: Colors.blue),
+              Container(color: Colors.red),
+              Container(color: Colors.green),
+              Container(color: Colors.blue),
+            ],
+          ),
+
+          ////////////////
+          SliverGrid.extent(
+            maxCrossAxisExtent: 200,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 4.0,
+            children: <Widget>[
+              Container(color: Colors.pink),
+              Container(color: Colors.pink),
+              Container(color: Colors.pink),
+              Container(color: Colors.indigo),
+              Container(color: Colors.indigo),
+              Container(color: Colors.orange),
+              Container(color: Colors.pink),
+              Container(color: Colors.indigo),
+              Container(color: Colors.orange),
+            ],
           ),
         ],
       ),
     );
   }
 }
-
+//
+// //////////////
+// Scaffold(
+// body: CustomScrollView(
+// slivers: [
+// SliverAppBar(
+// floating: false,
+// expandedHeight: 120.0,
+// flexibleSpace: FlexibleSpaceBar(
+// title: Text('SilverGrid AppBar'),
+// ),
+// ),
+// SliverGrid(
+// delegate: SliverChildBuilderDelegate(
+// (context, index) {
+// return Container(
+// alignment: Alignment.center,
+// color: Colors.teal[100 * (index % 9)],
+// child: Text('grid item $index'),
+// );
+// },
+// childCount: 10,
+// ),
+// gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+// crossAxisCount: 3,
+// mainAxisSpacing: 15,
+// crossAxisSpacing: 15,
+// childAspectRatio: 2.0,
+// ),
+// ),
+// SliverToBoxAdapter(
+// child: Container(
+// color: Colors.yellow,
+// padding: const EdgeInsets.all(8.0),
+// child: Text('Grid Header', style: TextStyle(fontSize: 24)),
+// ),
+// ),
+// SliverGrid.count(
+// crossAxisCount: 3,
+// mainAxisSpacing: 10.0,
+// crossAxisSpacing: 10.0,
+// childAspectRatio: 4.0,
+// children: <Widget>[
+// Container(color: Colors.red),
+// Container(color: Colors.green),
+// Container(color: Colors.blue),
+// Container(color: Colors.red),
+// Container(color: Colors.green),
+// Container(color: Colors.blue),
+// ],
+// ),
+// SliverGrid.extent(
+// maxCrossAxisExtent: 200,
+// mainAxisSpacing: 10.0,
+// crossAxisSpacing: 10.0,
+// childAspectRatio: 4.0,
+// children: <Widget>[
+// Container(color: Colors.pink),
+// Container(color: Colors.indigo),
+// Container(color: Colors.orange),
+// Container(color: Colors.pink),
+// Container(color: Colors.indigo),
+// Container(color: Colors.orange),
+// ],
+// ),
+// ],
+// )
+// );
